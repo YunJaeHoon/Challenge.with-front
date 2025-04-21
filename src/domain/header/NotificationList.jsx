443319274a64c92e272dff03feed9b4e07532b13
YunJaeHoon <sleepzzz214@naver.com>
2025-04-21 16:00:52 +0900

    fix: 알림 페이지네이션 방식을 스크롤에서 버튼 방식 변경

--- src/domain/header/NotificationList.jsx
+++ src/domain/header/NotificationList.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useRef, useState } from "react"
 import style from "./NotificationListStyle.module.css"
 import { sendApi } from "../../utils/apiUtil";
 
+import nextPageIcon from "../../assets/NextNotificationPageIcon.svg"
+
 const NotificationList = React.memo(() => {
 
   // State
@@ -9,61 +11,49 @@ const NotificationList = React.memo(() => {
   const [page, setPage] = useState(0);                            // 현재 페이지
   const [hasMorePage, setHasMorePage] = useState(true);           // 더 불러올 알림이 있는가?
   const [isFetching, setIsFetching] = useState(false);            // 알림을 불러오는 중인가?
+  const [isEmpty, setIsEmpty] = useState(false);                  // 알림이 존재하지 않는가?
   const observerRef = useRef(null);                               // IntersectionObserver를 위한 ref
 
   // 알림 리스트 불러오기
-  const getNotificationList = async () => {
-    if (isFetching || !hasMorePage) return;
-  
-    setIsFetching(true);
-  
-    try {
-      const data = await sendApi("/api/notification", "GET", true, {
-        page: page,
-        size: 1,
-        sort: "createdAt,desc"
-      });
-  
-      console.log("Fetched page:", page, data);
-  
-      if (data.content) {
-        setNotificationList((prev) => [...prev, ...data.content]);
-        setHasMorePage(!data.isLast);
+  useEffect(() => {
+    const getNotificationList = async () => {
+      setIsFetching(true);
+    
+      try {
+        const data = await sendApi("/api/notification", "GET", true, {
+          page: page,
+          size: 1,
+          sort: "createdAt,desc"
+        });
+    
+        if (data.content) {
+          setNotificationList((prev) => [...prev, ...data.content]);
+          setHasMorePage(!data.isLast);
+        }
+      } catch(error) {
+        const errorCode = error.response.data.code;
+
+        if(page === 0 && errorCode === "NOTIFICATION_NOT_FOUND")
+          setIsEmpty(true);
+        setHasMorePage(false);
+      } finally {
+        setIsFetching(false);
       }
-    } catch {
-      setHasMorePage(false);
-    } finally {
-      setIsFetching(false);
     }
-  }  
 
-  // 페이지 변경될 때마다 데이터 요청
-  useEffect(() => {
     getNotificationList();
-  }, [page]);
+  }, [page])
 
-  // IntersectionObserver 설정
-  useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      const entry = entries[0];
-      if (entry.isIntersecting && !isFetching && hasMorePage) {
-        setPage((prev) => prev + 1);
-      }
-    }, {
-      threshold: 0.7,
-    });
-  
-    const currentRef = observerRef.current;
-    if (currentRef) observer.observe(currentRef);
-  
-    return () => {
-      if (currentRef) observer.unobserve(currentRef);
-    };
-  }, [isFetching, hasMorePage]);
+  // 다음 페이지 불러오기 함수
+  const getNextNotificationList = () => {
+    if(hasMorePage) {
+      setPage(page + 1);
+    }
+  }
 
   return (
     <div id={style["main-container"]}>
-      <div id={style["list-container"]}>
+      <div ref={observerRef} id={style["list-container"]}>
 
         {/* 알림 리스트 */}
         {notificationList.map((notification, index) => 
@@ -74,17 +64,20 @@ const NotificationList = React.memo(() => {
             <div className={style["notification-date"]}>
               {new Date(notification.createdAt).toLocaleString()}
             </div>
-            {(hasMorePage || index !== notificationList.length - 1) && (
-              <div className={style["notification-line"]}></div>
-            )}
+            {
+              (index < notificationList.length - 1) ? <div className={style["notification-line"]}></div> : ""
+            }
 
           </div>
         )}
 
-        <div ref={observerRef} style={{ height: "30px" }} />
-
         {/* 알림 불러오는 중 */}
-        {isFetching && <div id={style["loading-notification-description"]}>잠시만 기다려주세요.</div>}
+        {
+          isEmpty ? <div id={style["empty-notification-description"]}>알림이 없습니다.</div> :
+          isFetching ? <div id={style["loading-notification-description"]}>잠시만 기다려주세요.</div> :
+          hasMorePage ? <img src={nextPageIcon} id={style["next-page-button"]} onClick={getNextNotificationList} /> :
+          ""
+        }
 
       </div>
     </div>
